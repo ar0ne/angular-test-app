9f6be550928e6c1505bb9a6162c98b5a508d12ea
ar0ne <CAMTbITAKOU@yandex.ru>
2019-04-08 16:04:21 +0300

    add base logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgRedux, NgReduxModule } from '@angular-redux/store';
+import { IAppState, rootReducer, INITIAL_STATE } from './store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BorderComponent } from './border/border.component';
+import { ValueComponent } from './value/value.component';
+import { ManagerComponent } from './manager/manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BorderComponent,
+    ValueComponent,
+    ManagerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgReduxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor (ngRedux: NgRedux<IAppState>) {
+    ngRedux.configureStore(rootReducer, INITIAL_STATE);
+  }
+}
